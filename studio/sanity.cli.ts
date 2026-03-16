36061336c6a17707f5a94d8b68dbd715e500a07c
yashx420 <yash50sinha@gmail.com>
2026-03-16 17:07:42 +0530

    chore: save sanity appId to config

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '78cxu1v9',
-    dataset: 'property'
+    dataset: 'property',
   },
   deployment: {
     /**
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+    appId: 'umhzbsnhpcekybc2k68wiv1c',
+  },
 })
